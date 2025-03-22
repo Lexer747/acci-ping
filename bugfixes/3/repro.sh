979b75583a427a2152f4be7b21b22aa13052810c
Lexer747 <Lexer747@gmail.com>
2025-03-22 12:41:52 +0000

    Another bug

---
+++ bugfixes/3/repro.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+relative=$(dirname "${BASH_SOURCE[0]}")
+# panic: check failed: (x, y) {-9223372036854775808, 23} [2025-03-22 12:20:26.966385845 +0000 GMT m=+0.017629409, 9.496615ms] coordinate out of terminal {W: 128 H: 26} bounds. Index: 82384 [recovered]
+# W: 128 H: 26
+acci-ping drawframe -term-size "26x128" -debug-strict "${relative}/panic.pings"
+
---
+++ bugfixes/testrunner.go
@@ -0,0 +1,15 @@
+// Use of this source code is governed by a GPL-2 license that can be found in the LICENSE file.
+//
+// Copyright 2025 Lexer747
+//
+// SPDX-License-Identifier: GPL-2.0-only
+
+package bugfixes
+
+import "testing"
+
+func TestBugfixes(t *testing.T) {
+	t.Parallel()
+	// TODO execute all the `repro.sh` scripts
+	// Ones containing PANIC should be asserted not to panic
+}

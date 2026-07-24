465b86f9e8104014cdfeffc3b5de36ba8e59dc2b
Lexer747 <Lexer747@gmail.com>
2026-07-25 00:03:10 +0100

    improve release process

--- README.md
+++ README.md
@@ -93,7 +93,7 @@ go!
 
 Or use:
 ```sh
-go install github.com/Lexer747/acci-ping@v1.0.3
+go install github.com/Lexer747/acci-ping@v1.0.4
 ```
 To build and install locally via the `go install` toolchain
 
--- releases/do-release.sh
+++ releases/do-release.sh
@@ -6,8 +6,6 @@
 #
 # SPDX-License-Identifier: GPL-2.0-only
 
-set -x
-
 ROOT=$(git rev-parse --show-toplevel)
 SCRIPT_DIR="$ROOT"/releases/
 TOOLS_DIR="$ROOT"/tools/
@@ -45,14 +43,41 @@ VERSION=$(cat "$SCRIPT_DIR"version.txt)
 
 AFTER_README="go install github.com/Lexer747/acci-ping@v$VERSION"
 
+echo "fixing README"
+sed "s\\$BEFORE_README\\$AFTER_README\\g" <README.md >README.tmp.md
+mv README.tmp.md README.md
+
+echo "committing: New release v$VERSION"
 git add "$SCRIPT_DIR"version.txt
+git add README.md
 git commit -m "New release v$VERSION"
 
 git tag -a "v$VERSION" -m "Tagged automatically by do-release.sh at $TIMESTAMP"
 
+echo "building release binaries"
 "$TOOLS_DIR"/build.sh sign
 
 cp "$ROOT/out/linux/amd64/acci-ping-linux-amd64" "$HOME/go/bin/acci-ping"
 
-sed "s\\$BEFORE_README\\$AFTER_README\\g" <README.md >README.tmp.md
-mv README.tmp.md README.md
+echo "about to push auto generated release notes and commit"
+echo "waiting for user input, check release commit if wanted"
+read -p "Continue (Y/n)?" CONT
+if [ "$CONT" = "n" ]; then
+  exit 0;
+fi
+
+git push
+git push --tags
+
+gh release create "v$VERSION" -t "v$VERSION" --generate-notes
+gh release upload "v$VERSION" \
+ "$ROOT/out/darwin/amd64/acci-ping-darwin-amd64"#acci-ping-darwin-amd64 \
+ "$ROOT/out/darwin/arm64/acci-ping-darwin-arm64"#acci-ping-darwin-arm64 \
+ "$ROOT/out/linux/amd64/acci-ping-linux-amd64"#acci-ping-linux-amd64 \
+ "$ROOT/out/linux/arm64/acci-ping-linux-arm64"#acci-ping-linux-arm64 \
+ "$ROOT/out/windows/amd64/acci-ping-windows-amd64"#acci-ping-windows-amd64 \
+ "$ROOT/out/darwin/amd64/darwin-amd64.sig"#darwin-amd64.sig \
+ "$ROOT/out/darwin/arm64/darwin-arm64.sig"#darwin-arm64.sig \
+ "$ROOT/out/linux/amd64/linux-amd64.sig"#linux-amd64.sig \
+ "$ROOT/out/linux/arm64/linux-arm64.sig"#linux-arm64.sig \
+ "$ROOT/out/windows/amd64/windows-amd64.sig"#windows-amd64.sig
